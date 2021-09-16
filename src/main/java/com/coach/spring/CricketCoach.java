21bc176e2f715fdba34e62a842cd1482df4af2d1
Caike Campana <csallesc@ucsd.edu>
2021-09-16 00:05:54 -0700

    Literal Injection

--- src/main/java/com/coach/spring/CricketCoach.java
+++ src/main/java/com/coach/spring/CricketCoach.java
@@ -3,8 +3,12 @@ package com.coach.spring;
 public class CricketCoach implements Coach {
     private FortuneService fortuneService;
 
+    // Private fields for literal injection
+    private String emailAddress;
+    private String team;
+
     // Create a no-arg constructor
-    public CricketCoach(){
+    public CricketCoach() {
         System.out.println("CricketCoach: inside no-arg constructor.");
     }
 
@@ -12,6 +16,26 @@ public class CricketCoach implements Coach {
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println("CricketCoach: inside setter method - setFortuneService.");
         this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress.");
+
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam.");
+
+        this.team = team;
     }
 
     @Override
--- src/main/java/com/coach/spring/SetterDemoApp.java
+++ src/main/java/com/coach/spring/SetterDemoApp.java
@@ -7,8 +7,14 @@ public class SetterDemoApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
         CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+        // Setter Injection
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getDailyFortune());
+
+        // Literal Injection
+        System.out.println(theCoach.getEmailAddress());
+        System.out.println(theCoach.getTeam());
+
         context.close();
     }
 }
